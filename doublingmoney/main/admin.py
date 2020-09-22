f4187d3f7107f3c9a6122697a5818cf714cf751b
SSez <>
2020-09-22 22:27:32 +0300

    couple book models

--- doublingmoney/main/admin.py
+++ doublingmoney/main/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Books, Book_reviews
 
-# Register your models here.
+admin.site.register(Books)
+admin.site.register(Book_reviews)
--- doublingmoney/main/models.py
+++ doublingmoney/main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+#from django.urls import reverse
 
-# Create your models here.
+class Books(models.Model):
+    name = models.CharField(max_length=64)
+    author = models.CharField(max_length=32)
+    description = models.CharField(max_length=128)
+    image = models.ImageField(default='default_book.jpg', upload_to='book_pics')
+    pub_date = models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return f'Book name: {self.name}, Author: {self.author}'
+
+class Book_reviews(models.Model):
+    review = models.CharField(max_length=512)
+    stars = models.IntegerField(default=0)
+    date = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.user.username} Reviews'
