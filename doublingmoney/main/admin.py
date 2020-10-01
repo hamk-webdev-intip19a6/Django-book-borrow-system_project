8ef6e41384fe15e119d60cb5c65116dc8bf40250
SSez <>
2020-10-02 01:21:33 +0300

    merge

--- doublingmoney/main/admin.py
+++ doublingmoney/main/admin.py
@@ -1,26 +1,23 @@
 from django.contrib import admin
 
-from .models import Book, Book_author, Review, Author, Inventory
-
-class BookAuthorInLine(admin.StackedInline):
-    model = Book_author
-    extra = 1
+from .models import Book, Review, Author, Inventory
 
 class BookAdmin(admin.ModelAdmin):
     fields = [
         'title', 'description', 'pub_date', 'image', 'rental_rate', 
-        'replacement_cost'
+        'replacement_cost', 'author'
     ]
-    inlines = [BookAuthorInLine]
+
     list_display = ('title', 'pub_date')
 
 class InventoryAdmin(admin.ModelAdmin):
     list_display = ('book', 'available')
 
-
+class AuthorAdmin(admin.ModelAdmin):
+    fields = ['first_name', 'last_name']
+    
 admin.site.register(Book, BookAdmin)
 admin.site.register(Inventory, InventoryAdmin)
 admin.site.register(Author)
 admin.site.register(Review)
-admin.site.register(Book_author)
 
--- doublingmoney/main/views.py
+++ doublingmoney/main/views.py
@@ -9,9 +9,9 @@ from django.views.generic import (
     #UpdateView,
     #DeleteView
 )
-from django.db.models import Count, Q
-
-from .models import Book, Book_author, Inventory, Author
+from django.db.models import Count, Q, OuterRef, Subquery, Value
+from django.db.models.functions import Concat
+from .models import Book, Inventory, Author
 
 
 class HomeView(ListView):
@@ -20,32 +20,31 @@ class HomeView(ListView):
 
     def get_queryset(self):
         # returns list of available books.IF many of the same book add copies count
-        q = Book.objects.filter(inventory__available=True)
-        q = q.annotate(copies=Count('title'))
+        q = Book.objects.filter(inventory__available=True).annotate(copies=Count('title'))
         return q
 
 class SearchResultView(ListView):
-    model = Book
     template_name = 'main/search_results.html'
     context_object_name = "book_list"
 
     def get_queryset(self):
         query = self.request.GET.get('q')
-        book_list = Book.objects.filter(
-            Q(title__icontains=query) 
-            | Q(book_author__author_id__first_name__icontains=query)
-            | Q(book_author__author_id__last_name__icontains=query)
-        ).distinct()
+        if query:
+            book_list = Book.objects.filter(
+                Q(title__icontains=query) 
+               # | Q(book_author__author_id__first_name__icontains=query)
+               # | Q(book_author__author_id__last_name__icontains=query)
+            ).annotate(copies=Count('inventory__available'))
+        else:
+            book_list = Book.objects.all().annotate(copies=Count('inventory__available'))
         return book_list
 
 def book(request, book):
     book = book
     book_title = Book.objects.filter(title = book)
     available = Book.objects.filter(inventory__available=True, title=book).annotate(copies=Count('title'))
-    authors = Author.objects.filter(book_author__book_id__title = book)
     context = {'book_title': book_title, 
-                'available': available, 
-                'authors': authors}
+                'available': available}
                 
     return render(request, 'main/book.html', context)
 
